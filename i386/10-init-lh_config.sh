399fe5197e99f99934ba371032d2f438251f94d3
Thiago Martins <thaigocmartinsc@gmail.com>
2010-03-19 22:09:04 -0300

    Packages adjusted.

--- i386/10-init-lh_config.sh
+++ i386/10-init-lh_config.sh
@@ -16,8 +16,8 @@ lh_config \
     --debconf-priority high \
     --cache enabled \
     --bootloader grub \
-    --linux-packages "linux-image-2.6 aufs-modules-2.6 squashfs-modules-2.6 redhat-cluster-modules-2.6" \
-    --packages "squashfs-tools aufs-tools libc6-xen bridge-utils xen-linux-system-2.6.26-2-xen-686 xen-hypervisor-3.2-1-i386 xen-utils-3.2-1 xen-docs-3.2 libvirt-bin less virt-manager virt-viewer xen-tools xorg x11-xserver-utils gnome-session gnome-panel gdm evince metacity gnome-terminal gnome-backgrounds gnome-themes nautilus tsclient mesa-utils vinagre gnome-system-monitor lvm2 mdadm dmraid iceweasel gedit kpartx parted gparted ntfsprogs ntfs-3g vim telnet screen vncviewer mlocate desktop-base vlan vtun vde2 vdetelweb redhat-cluster-suite aoetools vblade vblade-persist python-rpm yelp iscsitarget open-iscsi isns isns-client ssh iscsitarget-modules-2.6-686 iscsitarget-modules-2.6-xen-686 ganeti ganeti-instance-debootstrap firmware-bnx2 snmpd lighttpd php5-cgi php5-cli php5-sqlite sqlite3 kvm libwww-perl libdigest-sha1-perl" \
+    --linux-packages "linux-image-2.6 aufs-modules-2.6 squashfs-modules-2.6 redhat-cluster-modules-2.6 iscsitarget-modules-2.6" \
+    --packages "squashfs-tools aufs-tools libc6-xen bridge-utils xen-linux-system-2.6.26-2-xen-686 xen-hypervisor-3.2-1-i386 xen-utils-3.2-1 xen-docs-3.2 libvirt-bin less virt-manager virt-viewer xen-tools xorg x11-xserver-utils gnome-session gnome-panel gdm evince metacity gnome-terminal gnome-backgrounds gnome-themes nautilus tsclient mesa-utils vinagre gnome-system-monitor lvm2 mdadm dmraid iceweasel gedit kpartx parted gparted ntfsprogs ntfs-3g vim telnet screen vncviewer mlocate desktop-base vlan vtun vde2 vdetelweb redhat-cluster-suite aoetools vblade vblade-persist python-rpm yelp iscsitarget open-iscsi isns isns-client ssh ganeti ganeti-instance-debootstrap firmware-bnx2 snmpd lighttpd php5-cgi php5-cli php5-sqlite sqlite3 kvm libwww-perl libdigest-sha1-perl" \
     --chroot-filesystem squashfs \
     --union-filesystem aufs \
     --username livexen \
