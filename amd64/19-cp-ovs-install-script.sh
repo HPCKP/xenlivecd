a02884f76d21685a4e59f365f88afb3681e1be6c
Thiago Martins <thaigocmartinsc@gmail.com>
2010-03-27 00:37:52 -0300

    The amd64 arch was updated and some cosmetic changes was made.

--- amd64/19-cp-ovs-install-script.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-cp ../stuff/10-install-openvswitch-amd64.sh config/chroot_local-hooks
--- i386/19-cp-ovs-install-script.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-cp ../stuff/10-install-openvswitch-i386.sh config/chroot_local-hooks
